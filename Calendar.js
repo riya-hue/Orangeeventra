d427eed205293320620e832a5fcd600577922f9a
riya-hue <riyashr1510@gmail.com>
2025-12-31 21:36:41 +0530

    Create Calendar.js

---
+++ Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Calendar() {
+  return (
+    <div style={{ padding: "30px", textAlign: "center" }}>
+      <h1 style={{ color: "#cc5500" }}>Event Calendar</h1>
+      <p>Here you will see a calendar of all upcoming events.</p>
+    </div>
+  );
+}
+
+export default Calendar;
