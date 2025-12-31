6f4c0720c29193857219470224ce2baef1c3eab0
riya-hue <riyashr1510@gmail.com>
2025-12-31 21:37:23 +0530

    Create EventDetails.js

---
+++ src/pages/EventDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventDetails = () => {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h2 style={{ color: "#ff8800" }}>Event Details Page</h2>
+      <p>More details about the selected event will appear here.</p>
+    </div>
+  );
+};
+
+export default EventDetails;
